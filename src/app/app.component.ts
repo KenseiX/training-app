ae807418d1b36507772cd789067c060d7e81d535
KenseiX <kensei_x@hotmail.co.uk>
2018-04-22 15:07:37 +0100

    Added component, added + deleted to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Training app';
   creatorname = 'Mark Skeath';
+  counters = [];
 
   changeName() {
     this.creatorname = 'totally not mark';
   }
+
+  addLogin() {
+    this.counters.push('');
+  }
+
+  deleteLogin() {
+    if (this.counters.length > 0)
+    this.counters.pop();
+    else
+    alert ('Example Alert');
+  }
+
 }
