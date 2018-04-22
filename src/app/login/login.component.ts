6370bbbbe169523671dde98df42d0feff9c0e60e
KenseiX <kensei_x@hotmail.co.uk>
2018-04-22 16:02:34 +0100

    dummy functions for 2 buttons

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,15 @@ export class LoginComponent {
     email
     password
 
+    loginclick(){
+        console.log("this is your email" + this.email)
+    }
+
+    registerclick(){
+        console.log("This is your password" + this.password)
+    }
+
+
     print() {
         console.log(this.email)
         console.log(this.password)
