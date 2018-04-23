58749f35197d5aa490a4a207aca4d0ab865c4328
KenseiX <kensei_x@hotmail.co.uk>
2018-04-23 12:09:07 +0100

    removed some code from main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Training app';
+  title = 'Hockey app';
   creatorname = 'Mark Skeath';
   counters = [];
 
