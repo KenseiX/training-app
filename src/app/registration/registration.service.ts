fc016af45164591b0523b1ae56b9198d2b863fe5
KenseiX <kensei_x@hotmail.co.uk>
2018-05-30 20:50:09 +0100

    registration checks if email is available

--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -1,12 +1,51 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import { mergeMap } from 'rxjs/operator/mergeMap';
 
 @Injectable()
 
 export class RegistrationService{
 
-    dummyemail = "test@email.com"
-    dummypassword = "testpass"
+    //dummyemail = "test@email.com"
+    //dummypassword = "testpass"
 
+    login;
+
+    constructor (private http:Http){
+    };
+
+
+    checklogin (email){
+        this.getlogin(email);
+        
+        setTimeout(() => {
+            console.dir(this.login)
+            if ( !this.login){
+                console.log ("Email available")
+            }
+            else
+            {
+                console.log ("Email already exists")
+                window.alert("Email already exists");
+            }
+        }, 100)
+    }
+
+
+    getlogin (email){
+        // this.test = this.http.get(`api/${email}/getLoginData`).pipe(mergeMap(data ))
+        return this.http.get(`api/${email}/getLoginData`).subscribe(data => { 
+            this.login = data.json()
+            console.dir(this.login)
+        });
+    }
+
+
+
+    /*
     checklogin (email){
         if (email === this.dummyemail){
             console.log ("invalid email hit (use different email)")
@@ -19,4 +58,5 @@ export class RegistrationService{
     checkpassword(password){
 
     }
+    */
 }
